17fb7b5cc828760c60dddce8c0b1503b8cf76763
Rommel <cap03487@gmail.com>
2025-01-17 00:17:59 -0500

    fixing validation errors and format documents

--- chamber/scripts/displaydata.js
+++ chamber/scripts/displaydata.js
@@ -1,11 +1,11 @@
 
-const yearElement=document.getElementById("currentyear");
-const currentYear= new Date().getFullYear();
-yearElement.textContent= `© ${currentYear} | Rommel Juarez | Quito-Ecuador 🇪🇨`;
+const yearElement = document.getElementById("currentyear");
+const currentYear = new Date().getFullYear();
+yearElement.textContent = `© ${currentYear} | Rommel Juarez | Quito-Ecuador 🇪🇨`;
 
-const lastModifiedElement=document.getElementById("lastModified");
-const lastModified= document.lastModified;
-lastModifiedElement.textContent=`${lastModified}`;
+const lastModifiedElement = document.getElementById("lastModified");
+const lastModified = document.lastModified;
+lastModifiedElement.textContent = `${lastModified}`;
 
 
 const businessListDiv = document.getElementById('business-list');
@@ -13,12 +13,12 @@ const toggleViewButton = document.getElementById('toggle-view-btn');
 
 
 function renderBusinesses(businesses) {
-    businessListDiv.innerHTML = ''; 
+    businessListDiv.innerHTML = '';
 
     businesses.forEach(business => {
         const card = document.createElement('div');
         card.classList.add('business-card');
-        
+
         card.innerHTML = `
             <img src="${business.icon}" alt="${business.name} Logo">
             <h3>${business.name}</h3>
@@ -26,7 +26,7 @@ function renderBusinesses(businesses) {
             <p><strong>Address:</strong> ${business.address}</p>
             <p><strong>Phone:</strong> ${business.phone}</p>
         `;
-        
+
         businessListDiv.appendChild(card);
     });
 }
@@ -39,11 +39,11 @@ toggleViewButton.addEventListener('click', () => {
 
 async function fetchBusinesses() {
     try {
-        
+
         const response = await fetch('data/members.json');
         const data = await response.json();
 
-        
+
         renderBusinesses(data);
     } catch (error) {
         console.error("Error fetching businesses:", error);
